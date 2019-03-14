6620a2538c438244cfd568bf783895ca536231f1
nokranger <anusorn.thavornporn@gmail.com>
2019-03-14 15:36:43 +0700

    edit arduino task1 done

--- Arduino/HC_12_serial/HC_12_serial.ino
+++ Arduino/HC_12_serial/HC_12_serial.ino
@@ -27,25 +27,41 @@ void loop() {
         status = 1;
         if(status == 1){
         Serial1.begin(9600);
-        Serial1.println("Sta1");
+        Serial1.println("AD1 + S1");
         Serial1.end();
-        delay(500);
-        int mm = sensor.readRangeContinuousMillimeters();
-//        Serial.print(mm);
-        if (sensor.timeoutOccurred())Serial.print(" TIMEOUT"); 
-//        Serial.println("mm");
         delay(1000);
         Serial.println("Solid 1 open");
         delay(500);
         Serial.println("Solid 2 off");
         delay(500);
+        Serial.println("Solid 1 off");
         Serial.println("Waiting for calulate data");
+        delay(2000);
+        int mm[9];
+        int total = 0;
+        int count = 0;
+        for(int i = 0;i < 10;i++){
+          mm[i] = sensor.readRangeContinuousMillimeters();
+          total = total + mm[i];
+          Serial.print("Total : ");
+          Serial.print(i);
+          Serial.print(" = ");
+          Serial.println(total);
+          count++;
+        }
+        total = total / count;
+        Serial.print("Total : ");
+        Serial.println(total);
+        if (sensor.timeoutOccurred())Serial.print(" TIMEOUT"); 
+//        Serial.println("mm");
         delay(500);
-        Serial.print("AD1 + S1OP + S2OFF + WFCD3s + DS");
-        Serial.println(mm);
+        Serial.print("AD1 + DS");
+        Serial.print(total);
+        Serial.println(" +S1OFF + S2OFF + WFCD3s");
         Serial1.begin(9600);
-        Serial1.print("AD1 + S1OP + S2OFF + WFCD3s + DS");
-        Serial1.println(mm);
+        Serial1.print("AD1 + DS");
+        Serial1.print(total);
+        Serial1.println(" +S1OFF + S2OFF + WFCD3s");
         Serial1.end();
         delay(3000);
         Serial.println("Solid 1 off");
@@ -53,19 +69,21 @@ void loop() {
         Serial.println("Solid 2 on");
         delay(500);
         Serial1.begin(9600);
-        Serial1.println("S1OFF + S2ON");
+        Serial1.println("AD1 + S1OFF + S2ON");
         Serial1.end();
         delay(3000);
         status = 0;
         Serial1.begin(9600);
-        Serial1.println("END");
-        Serial.println("END");
+        Serial1.println("AD1 + S1OFF + S2OFF");
+        Serial1.println("AD1 + END");
+        Serial.println("AD1 + END");
         Serial1.end();
         Serial1.begin(9600);
         }
         }
       //Serial.println("this is serial read" + Serial1.read());
   }
+  
   // read from port 0, send to port 1:
   if (Serial.available()) {
       Serial1.write(Serial.read());
